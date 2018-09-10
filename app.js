f9e33d93916b1a7effbf546c5d6ad9f9341e43b9
mimiv192 <misha.vernon@yahoo.com>
2018-09-10 16:50:56 -0500

    kitchensink

---
+++ app.js
@@ -0,0 +1,83 @@
+//name variable
+let name = "Misha";
+//state constant
+US_STATES= "50";
+// sum 5 and 4
+let sum = 5 + 4;
+// check if name starts with L, respond based on result
+if (charCode (0) > 76) {
+    alert("Back of the Line!");
+
+} else { alert ("Next!")};
+//Hello World! function
+function sayHello ( ){
+    alert("Hello World!");
+}
+//call function
+sayHello ();
+//checkAge function
+function checkAge(name,age) {
+    if (age < 21) {
+        alert ("Sorry" + name + "you aren't old enough to view this page!")
+    }
+} 
+//Make friends and run through function
+let Charles=;{  
+name: "Charles"
+age:"21"
+}
+
+let Abby=;{
+name: "Abby"
+age: "27"
+}
+
+let James=;{
+name: "James"
+age: "18"    
+}
+
+let John=;{
+name: "John"
+age: "18"
+}
+checkAge(charles.name, charles.age);
+checkAge(abby.name, abby.age);
+checkAge(james.name, james.age);
+checkAge(john.name, john.age);
+//array of favorite vegetables
+var vegetables1= "asparagus";
+var vegetables2= "carrots";
+var vegetables3= "lentils";
+for ([let 0=i, i<vegetables; i++
+console.log vegetables(i)])
+//Array of 5 friends
+let friends= [
+ {name: Paul 
+    age: 21}
+  {name: Rusty
+    age:27} 
+  {name: Brian 
+    age: 19}
+   {name: Abe 
+    age: 17} 
+{name: Wix 
+    age: 41}    
+    
+]
+//run friends through loop
+for (let i=0, i < friends.length; i++) {
+    checkAge(friends[i].name friends[i].age);
+}
+// getLength function
+function getLength (string)
+return string.length;
+}
+// cal function
+let length = getLength ("Hello World")
+// check odd/even on length
+if (length % 2===0) {
+    console.log ("The world is nice nad even") 
+} else {
+    console.log ("The world is an odd place")
+}
